3708226ec7a302bd58f27360753078e1df242315
tdobber <timo.dobber@student.uva.nl>
2021-09-21 20:13:52 +0200

    Add solution for day 1 2015

---
+++ 2015/Days/Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.Helpers;
+
+namespace AdventOfCode._2015
+{
+    public static class Day1
+    {
+        private static string Input => InputHelper.GetInputString(2015, 1);
+
+        private static int Part1() =>
+            Input.Count(x => x == '(') - Input.Count(x => x == ')');
+
+        private static int Part2()
+        {
+            int currentFloor = 0;
+            for (int i = 0; i < Input.Length; i++)
+            {
+                if (Input[i] == '(')
+                {
+                    currentFloor++;
+                }
+                else if (Input[i] == ')')
+                {
+                    currentFloor--;
+                }
+
+                if (currentFloor == -1)
+                {
+                    return i + 1;
+                }
+            }
+
+            return 0;
+        }
+
+        public static void Start()
+        {
+            Console.WriteLine($"The answer to part 1 is floor {Part1()}.");
+            Console.WriteLine($"The answer to part 2 is position {Part2()}.");
+        }
+    }
+}
